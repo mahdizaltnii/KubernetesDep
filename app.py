13be6f42ad415eed7dff7e6fdb8768eb202df832
mahdizaltnii <98525165+mahdizaltnii@users.noreply.github.com>
2022-10-06 16:42:16 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello Mahdi you completed phase 1 of this project Flask , Now lets start phase 2 !!!'
+    return 'Hello Mahdi you completed phase 1 of this project Flask ,kkkkkkkkkkkkkkk !!!'
